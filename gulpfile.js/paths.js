1d32e7c0648cf7a24a9b2956746ebe21c0dbeb08
richbart14 <r87bart@gmail.com>
2021-07-14 21:35:45 +0200

    Aggiunto costruttore di percorsi path. Modificate le stringhe dei percorsi con i metodi definiti dentro path.

---
+++ gulpfile.js/paths.js
@@ -0,0 +1,46 @@
+//FILE CHE CI PERMETTE DI GESTIRE IN UN UNICO PUNTO TUTTI I PERCORSI DELLA NOSTRA APPLICAZIONE
+//DEFINISCO ALL'INTERNO TUTTI I PERCORSI DELLL'APPLICAZIONE
+//ci basterà cambiare da quì i percorsi e il tutto verrà aggiornato anche nei file che utilizzano le relative funzioni
+
+const paths = {
+    //definisco percorsi, global riguarda le routes, cioè la cartella src e quella di destinazione dist
+    global: {
+        src: "./src",
+        dist: "./dist"
+    },
+    //creo sezioni gestione html e js.
+    html: {
+        entry: "index.html",  //entry file
+        dist: "index.html"   //destinazione, inteso solo come nome file e non come percorso di destinazione quindi nome è uguale
+    },
+    js: {
+        entry: "index.js", //entry file
+        base: "js",
+        dist: "js"         //cartella di destinazione, dove vogliamo andare ad inserire questi file
+    }
+}
+
+module.exports = {
+    //funzioni che si incarica di gestire composizione dei percorsi file
+    getDistFolder: function() {
+        return paths.global.dist; //accedo a questi valori attraverso funzione
+    },
+    getSrcFolder: function() {
+        return paths.global.src; //restituisce percorso folder src
+    },
+    getHTMLEntryPath: function() {
+        return paths.global.src + '/' + paths.html.entry; 
+    },
+    getJsEntryPath: function() {
+        return paths.global.src + '/' + paths.js.base + '/' + paths.js.entry;
+    },
+    getJsSrcPath: function(innerPath) {
+        const baseJSPath = paths.global.src + '/' + paths.js.base;
+        //se c'è un innerPath (parametro) mi restituisci la const base + il parametro
+        if(innerPath) { 
+            return baseJSPath + '/' + innerPath;
+        } 
+        //altrimenti solo la const base con il percorso lì dentro definito
+        return baseJSPath;
+    } 
+}
--- gulpfile.js/viewTasks.js
+++ gulpfile.js/viewTasks.js
@@ -4,20 +4,23 @@ const gulp = require("gulp");
 const inject = require("gulp-inject"); /*installo gulp inject, è UN PLUGIN DI GULP, che ci permette di inserire all'interno
                                         di un file quello che vogliamo, la stringa che vogliamo (stringa script src)
                                         npm i gulp-inject -D*/
-
+const paths = require("./paths"); //IMPORTO PATHS che è il file dove gestisco i percorsi dell'applicazione
+                                    
 //CREO FUNZIONE ANONIMA compileIndex
 const compileIndex = function() {
     //definisco istruzione al suo interno: conterrà il cosiddetto PIPELINE, il processo a cascata che contraddistingue GULP
     
     /*una volta installato pacchetto gulp-inject definisco file in entrata attraverso gulp.src
     che andrò ad utilizzare nel primo pipe con metodo inject */
-    const jsIndex = gulp.src("./src/js/index.js");
-    const utilsIndex = gulp.src("./src/js/utils.js");
-    const modelsIndex = gulp.src("./src/js/models/Wallet.js");
+    //const jsIndex = gulp.src("./src/js/index.js");
+    const jsIndex = gulp.src(paths.getJsEntryPath()); //UTILIZZO PERCORSI GENERATI CON PATH E NON LA STRINGA
+    const utilsIndex = gulp.src(paths.getJsSrcPath("/utils.js"));
+    const modelsIndex = gulp.src(paths.getJsSrcPath("/models/Wallet.js"));
 
     /*quello che andremo a restituire sarà sempre un qualcosa legato a gulp,
     parto da un file di entrata che sarà index.html*/
-    return gulp.src("./src/index.html")
+    //return gulp.src("./src/index.html")
+    return gulp.src(paths.getHTMLEntryPath()) //UTILIZZO PERCORSI GENERATI CON PATH E NON LA STRINGA
     //definisco cosa voglio fare con questo index.html utilizzando i metodi pipe()
         .pipe(inject(jsIndex, { relative: true, name: "custom" })) //lo inserisco all'interno di una sezione che chiamo CUSTOM
         /*inserisco sezione in index.html attraverso <!-- custom:js --> <!-- endinject -->
@@ -27,8 +30,9 @@ const compileIndex = function() {
         .pipe(inject(modelsIndex, { relative: true, name: "custom:models" }))
                                     //mettendo relative il percorso sarà relativo alla cartella dist, importante
 
-    //ed il'ultimo pipe() si incaricherà di inserire all'interno della nostra destinazione il file modificato
-        .pipe(gulp.dest("./dist")); //destinazione sarà cartella dist
+    //e l'ultimo pipe() si incaricherà di inserire all'interno della nostra destinazione il file modificato
+        //.pipe(gulp.dest("./dist")); //destinazione sarà cartella dist
+        .pipe(gulp.dest(paths.getDistFolder())); //utilizzo funzione invece della stringa perchè leggo percorso da file paths che ho importato
 }
 
 /*PER AVVIARE GULP DEVO INSERIRE NEGLI scripts DEL package.json il processo che permette di avviare l'utilità di gulp.
@@ -39,7 +43,7 @@ che compilerà il nostro index.html dentro cartella DIST */
 
 //IMPLEMENTO FUNZIONE DI WATCH ANCHE PER il file index.html
 const watchIndex = function(cb) {
-    gulp.watch("./src/index.html", compileIndex)
+    gulp.watch(paths.getHTMLEntryPath(), compileIndex); //UTILIZZO PERCORSI GENERATI CON PATH E NON LA STRINGA
     cb();
 }
 
