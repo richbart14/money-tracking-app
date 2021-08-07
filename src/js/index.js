37758d9ec7b60032096e6f00f716ce844f30040c
richbart14 <r87bart@gmail.com>
2021-08-07 17:55:18 +0200

    Creata funzione per aggiungere lista operazioni all'interno della tabella.

--- src/js/index.js
+++ src/js/index.js
@@ -58,6 +58,7 @@
         try { //aggiungo operazione al WALLET, OPERAZIONE VIENE AGGIUNTA NEL LOCAL STORAGE
             wallet.addOperation(operation); //vedo op in entrata e la passo ad addOperation nel costruttore wallet
             updateBalance(); //invoco funzione per aggiornare il saldo quando aggiungo un operazione
+            updateOperationsTable(); //aggiorno lista operazioni quando un operazione viene aggiunta
             toggleModal(); //se operazione va a buon fine richiamo toggleModal per chiudere il pannello di aggiunta operazione
             ev.target.reset(); //resetto i campi del form con metodo RESET di html 5 per i form
             showMessage('Operation added succesfully!', Enums.SnackbarTypes.SUCCESS); //chiamo funzione con messaggio per il toast
@@ -111,6 +112,49 @@
         balanceElement.textContent = getBalance();
     }
 
+    const updateOperationsTable = function() { //FUNZIONE PER AGGIORNARE TABELLA CON LE OPERAZIONI
+        const operations = Array.from(getOperations()); ///ottengo le operazioni
+        /*utilizzo Array.from per non intaccare array originale, visto che poi dovrò usare metodo reverse()
+        su array operations per permettere lettura delle operazioni dalla più recente alla meno recente,
+        metodo reverse cambierebbe altrimenti ordine array originale, in questo modo lo cambio solo qua in questa funzione*/
+        const tableElement = document.getElementById('table-body'); //prendo la tabella
+        if(!tableElement) { //controllo sempre che esista altrimenti termino istruzione con return
+            return;
+        }
+        tableElement.innerHTML = ''; //pulizia completa di tableElement qualora dovessimo avere al suo interno dei valori, RICREO TUTTO DINAMICAMENTE
+        operations.reverse().forEach(function(operation) { //iterazione sulle operazioni (item, singola operazione)
+            //Aggiungo operazione alla tabella iterando operations con un forEach e CREO TABLE-ROW CON LE 4 CELLE (description-amount-date-delete)
+            //CREO TABLE-ROW, inserisco nome tag da creare 'tr'
+            const trRow = document.createElement('tr');
+            //Specifo attributo per la table-row, ovvero che tipo di operazione è, expense o income, lo specifico in minuscolo
+            trRow.setAttribute('data-op-type', operation.type.toLowerCase()); 
+            //CREO LE 4 CELLE 'td', (description-amount-date-delete)
+            const tdDescription = document.createElement('td');
+            tdDescription.textContent = operation.description; //all'interno di tdDescription aggiungo come textContent operation.description
+            const tdAmount = document.createElement('td');
+            tdAmount.className = 'operation-amount'; //aggiungo classe css per il cambio dello stile dell'informazione dell'operazione
+            tdAmount.textContent = operation.amount; //all'interno di tdAmount aggiungo come textContent operation.amount
+            const tdDate = document.createElement('td');
+            tdDate.textContent = new Date(operation.date).toLocaleString(); //non può essere operation.date perchè date è espresso in millisecondi
+                        //serve ricostuire data con new Date, toLocaleString trasforma la data in una stringa basata sul Locale dell'utente in base al suo browser
+                        //nel caso di una piattaforma multilingua bisogna formattare la data a seconda se sia rivolta ad un italiano o ad un inglese ecc
+            const tdAction = document.createElement('td');
+            tdAction.className = 'align-text-center'; //aggiungo classe css alla cella td, le avevo create nell'html e css (helper)
+            const actionButton = document.createElement('button'); //creo il pulsante button
+            actionButton.className = 'button button-icon button-animated icon-delete'; //aggiungo al button le classi css che avevo creato
+            actionButton.onclick = function() { //aggiungo funzione di rimozione al button con evento onclick 
+                removeOperation(operation.id); //chiamo funzione removeOperation con operation.id parametro da rimuovere, OPERATION è IL PARAMETRO CHE IL FOREACH CI PASSA
+            }
+            tdAction.appendChild(actionButton); //appendo button a tdAction
+            //AGGIUNGO OGNI CELLA 'td' CREATA ALL'INTERNO DI 'trRow', E POI trRow ALL'INTERNO DELLA 'tableElement'
+            trRow.appendChild(tdDescription);
+            trRow.appendChild(tdAmount);
+            trRow.appendChild(tdDate);
+            trRow.appendChild(tdAction);
+            tableElement.appendChild(trRow);
+        });
+    }
+
     window.hideSnackbar = hideSnackbar;
     window.addOperation = addOperation;
     window.toggleModal = toggleModal; /*aggiungo funzione toggleModal al window perchè deve avere scope globale e perchè 2 pulsanti devono
@@ -124,5 +168,6 @@
         wallet = new Wallet(); //creazione variabile wallet che sarà la nuova istanza del costruttore Wallet
         updateBalance(); //invoco funzione per aggiornare il saldo quando carico la pagina
         //console.log(wallet);  LOG PROVA per vedere class wallet con le sue funzioni
+        updateOperationsTable(); //richiamo updateOperationsTable al lancio della piattaforma
     });
 //})();
